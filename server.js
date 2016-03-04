70632d22417ba588daa17ffb20a6d2868b1ef63c
Benjamin LaCara <ben.lacara@gmail.com>
2016-03-04 11:35:06 -0800

    socket io hooks

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ var PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
 
-
-http.listen(PORT, function() {
+http.listen(PORT, function(socket) {
     console.log('Server started!');
 });
